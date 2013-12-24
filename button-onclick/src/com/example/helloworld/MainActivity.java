c2e31cd46c9b5b92a21bcc6dcd6312eca0cfe3aa
sushant-hiray <hiraysushant@gmail.com>
2013-12-25 01:20:50 +0530

    Added a Simple button example

---
+++ button-onclick/src/com/example/helloworld/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.helloworld;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+	Button button;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		addListenerOnButton();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public void addListenerOnButton() {
+
+		button = (Button) findViewById(R.id.button1);
+
+		button.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+
+				Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.wncc-iitb.org"));
+				startActivity(browserIntent);
+
+			}
+
+		});
+
+	}
+
+
+}
