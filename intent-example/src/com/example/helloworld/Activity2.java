2f9dc7236c597fb1cc534c718cb68b23ba006cbc
sushant-hiray <hiraysushant@gmail.com>
2013-12-25 01:50:16 +0530

    Added a simple intent to shift from one activity screen to another

---
+++ intent-example/src/com/example/helloworld/Activity2.java
@@ -0,0 +1,14 @@
+package com.example.helloworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Activity2 extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main2);
+	}
+}
---
+++ intent-example/src/com/example/helloworld/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.helloworld;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+	Button button;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		addListenerOnButton();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public void addListenerOnButton() {
+		
+		final Context context=this;
+		button = (Button) findViewById(R.id.button1);
+
+		button.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				
+				Intent intent = new Intent(context,Activity2.class);
+                startActivity(intent);
+
+			}
+
+		});
+
+	}
+
+
+}
