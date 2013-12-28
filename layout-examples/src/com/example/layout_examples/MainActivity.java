9fdacca01b9506108ba9a83153cab0b674b18699
sushant-hiray <hiraysushant@gmail.com>
2013-12-28 10:44:27 +0530

    Added example for horizontal and vertical linear layout

---
+++ layout-examples/src/com/example/layout_examples/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.layout_examples;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+	Button button1;
+	Button button2;
+	Button button3; //covers the entire remaining width of the screen. check weight=1 in layout
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public void goVertical(View V)
+    {
+            // Create The  Intent and Start The Activity
+            Intent intentGetMessage=new Intent(this,Vertical.class);
+            startActivity(intentGetMessage);// Activity is started
+    }
+
+}
